a4796df53e0fcefdfafd7761d2c53b16af82b0d4
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-05-24 20:54:15 -0300

    Finalização do projeto

--- src/componentes/Time/index.tsx
+++ src/componentes/Time/index.tsx
@@ -1,35 +1,35 @@
-import { IColaborador } from "../../compartilhado/interfaces/IColaborador";
-import Colaborador from "../Colaborador";
-import "./Time.css";
+import React from 'react'
+import { IColaborador } from '../../compartilhado/interfaces/IColaborador'
+import Colaborador from '../Colaborador'
+import './Time.css'
 
-interface TimeProps{
+interface TimeProps {
     corPrimaria: string
+    corSecundaria: string
     nome: string
     colaboradores: IColaborador[]
-    corSecundaria: string
 }
 
-const Time = ({corPrimaria, nome, colaboradores, corSecundaria} :TimeProps) => {
-  const css = { backgroundColor: corSecundaria };
+const Time = (props: TimeProps) => {
+    const css = { backgroundColor: props.corSecundaria }
 
-  return colaboradores.length > 0 ? (
-    <section className="time" style={css}>
-      <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
-      <div className="colaboradores">
-        {colaboradores.map((colaborador) => (
-          <Colaborador
-            corDeFundo={corPrimaria}
-            key={colaborador.nome}
-            nome={colaborador.nome}
-            cargo={colaborador.cargo}
-            imagem={colaborador.imagem}
-          />
-        ))}
-      </div>
-    </section>
-  ) : (
-    ""
-  );
-};
+    return (
+        (props.colaboradores.length > 0) ? <section className='time' style={css}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => 
+                    <Colaborador 
+                        corDeFundo={props.corPrimaria} 
+                        key={colaborador.nome} 
+                        nome={colaborador.nome} 
+                        cargo={colaborador.cargo} 
+                        imagem={colaborador.imagem}
+                        data={colaborador.data}
+                    /> )}
+            </div>
+        </section> 
+        : <></>
+    )
+}
 
-export default Time;
+export default Time
